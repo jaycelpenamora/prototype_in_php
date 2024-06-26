2a92647ba38a8ab14b816d9edb3522b6909e3684
jaycelpenamora <jaycelpenamora@gmail.com>
2024-06-26 13:43:56 +0800

    modified:   src/App/views/login.php

--- src/App/views/login.php
+++ src/App/views/login.php
@@ -1,5 +1,8 @@
 <?php include $this->resolve("partials/_header.php"); ?>
 
-<p>Login</p>
+<section class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
 
+  <p>Login</p>
+
+</section>
 <?php include $this->resolve("partials/_footer.php"); ?>
