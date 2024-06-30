f867a992a7a7c2a3db59f814bfe6c96fc21337fc
jaycelpenamora <jaycelpenamora@gmail.com>
2024-07-01 02:23:44 +0800

    modified:   src/App/Services/ValidatorService.php new file:   src/Framework/Contracts/RuleInterface.php new file:   src/Framework/Rules/RequiredRule.php modified:   src/Framework/Validator.php

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Validator;
+use Framework\Rules\RequiredRule;
 
 class ValidatorService
 {
@@ -13,10 +14,23 @@ class ValidatorService
     public function __construct()
     {
         $this->validator = new Validator();
+
+        $this->validator->add('required', new RequiredRule());
     }
 
     public function validateRegister(array $data): void
     {
-        $this->validator->validate($data);
+        $this->validator->validate(
+            $data,
+            [
+                'email' => ['required'],
+                'age' => ['required'],
+                'country' => ['required'],
+                'social_media_url' => ['required'],
+                'password' => ['required'],
+                'confirm_password' => ['required'],
+                'tos' => ['required']
+            ]
+        );
     }
 }
--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -4,11 +4,34 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Contracts\RuleInterface;
+
 class Validator
 {
-    public function validate(array $data): void
+    private array $rules = [];
+
+    public function add(string $alias, RuleInterface $rule)
     {
-        dd($data);
+        $this->rules[$alias] = $rule;
     }
 
+    public function validate(array $data, array $fields): void
+    {
+        $errors = [];
+        foreach ($fields as $field => $rules) {
+            foreach ($rules as $rule) {
+                $ruleValidator = $this->rules[$rule];
+
+                if ($ruleValidator->validate($data, $field, [])) {
+                    continue;
+                }
+
+                $errors[$field][] = $ruleValidator->getMessage($data, $field, []);
+            }
+        }
+
+        if (count($errors)) {
+            dd($errors);
+        }
+    }
 }
