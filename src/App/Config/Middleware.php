e9369d07aa97fd5a623b9a02e3ff7182740f2b83
jaycelpenamora <jaycelpenamora@gmail.com>
2024-07-02 20:28:21 +0800

    modified:   src/App/Config/Middleware.php new file:   src/App/Middleware/FlashMiddleware.php modified:   src/App/views/register.php modified:   src/Framework/Rules/RequiredRule.php

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -8,9 +8,11 @@ use Framework\App;
 use App\Middleware\TemplateDataMiddleware;
 use App\Middleware\ValidationExceptionMiddleware;
 use App\Middleware\SessionMiddleware;
+use App\Middleware\FlashMiddleware;
 
 function registerMiddleware(App $app){
     $app->addMiddleware(TemplateDataMiddleware::class);
     $app->addMiddleware(ValidationExceptionMiddleware::class);
+    $app->addMiddleware(FlashMiddleware::class);
     $app->addMiddleware(SessionMiddleware::class);
 }
