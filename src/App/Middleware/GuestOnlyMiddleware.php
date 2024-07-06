8bb25881f295fbd5752a3a0bc9c43929a01a4cf4
jaycelpenamora <jaycelpenamora@gmail.com>
2024-07-07 02:24:48 +0800

    new file:   src/App/Middleware/GuestOnlyMiddleware.php

---
+++ src/App/Middleware/GuestOnlyMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Framework\Contracts\MiddlewareInterface;
+
+class GuestOnlyMiddleware implements MiddlewareInterface
+{
+  public function handle(callable $next):void
+  {
+    if (!empty($_SESSION['user'])) {
+      redirect('/');
+    }
+
+    $next();
+  }
+}
+
