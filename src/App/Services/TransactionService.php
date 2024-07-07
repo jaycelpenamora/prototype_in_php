86bf8343e71a8dd82b14e2359d646f68bc004086
jaycelpenamora <jaycelpenamora@gmail.com>
2024-07-07 17:13:26 +0800

    modified:   src/App/Services/TransactionService.php modified:   src/App/views/index.php

--- src/App/Services/TransactionService.php
+++ src/App/Services/TransactionService.php
@@ -10,94 +10,23 @@ class TransactionService
 {
   public function __construct(private Database $db) {}
 
-  // public function create(array $formData): void
-  // {
-  //
-  //   $currentDate = new \DateTime();
-  //   $currentDate->modify('+1 month');
-  //   $dueDate = $currentDate->format('Y-m-d');
-  //   // $this->db->query(
-  //   //   "INSERT INTO transactions(user_id, description, amount, date)
-  //   //   VALUES(:user_id, :description, :amount, :date)",
-  //   //   [
-  //   //     'user_id' => $_SESSION['user'],
-  //   //     'description' => $formData['description'],
-  //   //     'amount' => $formData['amount'],
-  //   //     'date' => $formattedDate
-  //   //   ]
-  //   // );
-  // }
-
-  // public function getUserTransactions(int $length, int $offset): array
-  // {
-  //   $searchTerm = addcslashes($_GET['s'] ?? '', '%_');
-  //   $params = [
-  //     'user_id' => $_SESSION['user'],
-  //     'description' => "%{$searchTerm}%"
-  //   ];
-  //
-  //   $transactions = $this->db->query(
-  //     "SELECT *, DATE_FORMAT(date, '%Y-%m-%d') as formatted_date
-  //     FROM transactions 
-  //     WHERE user_id = :user_id
-  //     AND description LIKE :description
-  //     LIMIT {$length} OFFSET {$offset}",
-  //     $params
-  //   )->findAll();
-  //
-  //   $transactions = array_map(function (array $transaction) {
-  //     $transaction['receipts'] = $this->db->query(
-  //       "SELECT * FROM receipts WHERE rental_id = :rental_id",
-  //       ['rental_id' => $transaction['id']]
-  //     )->findAll();
-  //
-  //     return $transaction;
-  //   }, $transactions);
-  //
-  //   $transactionCount = $this->db->query(
-  //     "SELECT COUNT(*)
-  //     FROM transactions 
-  //     WHERE user_id = :user_id
-  //     AND description LIKE :description",
-  //     $params
-  //   )->count();
-  //
-  //   return [$transactions, $transactionCount];
-  // }
-
-
-  // public function update(array $formData, int $id): void
-  // {
-  //   $formattedDate = "{$formData['date']} 00:00:00";
-  //
-  //   $this->db->query(
-  //     "UPDATE transactions
-  //     SET description = :description,
-  //       amount = :amount,
-  //       date = :date
-  //     WHERE id = :id
-  //     AND user_id = :user_id",
-  //     [
-  //       'description' => $formData['description'],
-  //       'amount' => $formData['amount'],
-  //       'date' => $formattedDate,
-  //       'id' => $id,
-  //       'user_id' => $_SESSION['user']
-  //     ]
-  //   );
-  // }
-
-  // public function delete(int $id): void
-  // {
-  //   $this->db->query(
-  //     "DELETE FROM transactions WHERE id = :id AND user_id = :user_id",
-  //     [
-  //       'id' => $id,
-  //       'user_id' => $_SESSION['user']
-  //     ]
-  //   );
-  // }
-
+  public function create(array $formData): void
+  {
+    $currentDate = new \DateTime();
+    $currentDate->modify('+1 month');
+    $dueDate = $currentDate->format('Y-m-d');
+    $this->db->query(
+      "INSERT INTO transactions(user_id, description, amount, date)
+      VALUES(:user_id, :description, :amount, :date)",
+      [
+        'user_id' => $_SESSION['user'],
+        'description' => $formData['description'],
+        'amount' => $formData['amount'],
+        'date' => $dueDate
+      ]
+    );
+  }
+  
   public function getMovies(): mixed
   {
     return $this->db->query("SELECT *, DATE_FORMAT(release_date, '%Y-%m-%d') as formatted_date FROM movies_T")->findAll();
--- src/App/views/index.php
+++ src/App/views/index.php
@@ -5,12 +5,11 @@
   <div class="flex items-center justify-between border-b border-gray-200 pb-4">
     <h3 class="font-medium">Catalog</h3>
     <div class="flex space-x-4">
-      <a href="/rental" class="flex items-center p-2 bg-sky-50 text-xs text-sky-900 hover:bg-sky-500 hover:text-white transition rounded">
+      <a href="/transaction" class="flex items-center p-2 bg-sky-50 text-xs text-sky-900 hover:bg-sky-500 hover:text-white transition rounded">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-
-        New Transaction
+        New Rental
       </a>
     </div>
   </div>
@@ -31,7 +30,7 @@
           Rentals
         </th>
         <th class="p-4 text-left text-sm font-semibold text-gray-900">
-          Bill
+          Billing
         </th>
         <th class="p-4 text-left text-sm font-semibold text-gray-900">
           Receipt(s)
@@ -56,24 +55,24 @@
           </td>
           <!-- Receipt List -->
           <td class="p-4 text-sm text-gray-600">
-            <!-- <?php foreach ($rental[''] as $receipt) : ?> -->
-            <!--   <div class="inline-block relative cursor-pointer"> -->
-            <!--     <a href="/rental/<?php echo e($rental['rental_id']); ?>/receipt/<?php echo e($receipt['rental_id']); ?>"> -->
-            <!--       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(109 40 217)" class="w-10 h-10"> -->
-            <!--         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" /> -->
-            <!--       </svg> -->
-            <!--     </a> -->
-            <!--     <form action="/rental/<?php echo e($rental['rental_id']); ?>/receipt/<?php echo e($receipt['rental_id']); ?>" method="POST"> -->
-            <!--       <?php include $this->resolve("partials/_csrf.php"); ?> -->
-            <!--       <input type="hidden" name="_METHOD" value="DELETE" /> -->
-            <!--       <button type="submit" class="absolute -top-1 -right-1"> -->
-            <!--         <svg xmlns="http://www.w3.org/2000/svg" fill="rgb(239 68 68)" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6"> -->
-            <!--           <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /> -->
-            <!--         </svg> -->
-            <!--       </button> -->
-            <!--     </form> -->
-            <!--   </div> -->
-            <!-- <?php endforeach; ?> -->
+            <?php foreach ($rental[''] as $receipt) : ?>
+              <div class="inline-block relative cursor-pointer">
+                <a href="/rental/<?php echo e($rental['rental_id']); ?>/receipt/<?php echo e($receipt['rental_id']); ?>">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(109 40 217)" class="w-10 h-10">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                  </svg>
+                </a>
+                <form action="/rental/<?php echo e($rental['rental_id']); ?>/receipt/<?php echo e($receipt['rental_id']); ?>" method="POST">
+                  <?php include $this->resolve("partials/_csrf.php"); ?>
+                  <input type="hidden" name="_METHOD" value="DELETE" />
+                  <button type="submit" class="absolute -top-1 -right-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="rgb(239 68 68)" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                  </button>
+                </form>
+              </div>
+            <?php endforeach; ?>
           </td>
           <!-- Date -->
           <td class="p-4 text-sm text-gray-600">
