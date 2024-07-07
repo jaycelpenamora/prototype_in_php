ab681e3646a720df31607a810df4b077a98eed9f
jaycelpenamora <jaycelpenamora@gmail.com>
2024-07-08 02:48:24 +0800

    modified:   sql_editor.sql

--- sql_editor.sql
+++ sql_editor.sql
@@ -1,9 +1,29 @@
-INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`) VALUES (NULL, 'Inception', 'English', 'Science Fiction', '2010-07-16 00:00:00', 399.00);
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Inception', 'English', 'Science Fiction', '2010-07-16 00:00:00', 399.00,'https://i.ebayimg.com/images/g/TAQAAOSwBt5ZIAiW/s-l1200.jpg');
 
-INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`) VALUES (NULL, 'Amélie', 'French', 'Romantic Comedy', '2001-04-25 00:00:00', 299.00);
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Amélie', 'French', 'Romantic Comedy', '2001-04-25 00:00:00', 299.00, 'https://m.media-amazon.com/images/I/714roYD1PYL._AC_UF894,1000_QL80_.jpg' );
 
-INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`) VALUES (NULL, 'Spirited Away', 'Japanese', 'Animation', '2001-07-20 00:00:00', 349.00);
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Spirited Away', 'Japanese', 'Animation', '2001-07-20 00:00:00', 349.00, 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg');
 
-INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`) VALUES (NULL, 'The Godfather', 'English', 'Crime', '1972-03-24 00:00:00', 499.00);
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'The Godfather', 'English', 'Crime', '1972-03-24 00:00:00', 499.00, 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg');
 
-INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`) VALUES (NULL, 'Parasite', 'Korean', 'Thriller', '2019-05-30 00:00:00', 399.00);
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Parasite', 'Korean', 'Thriller', '2021-05-30 00:00:00', 399.00, 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'The Dark Knight', 'English', 'Action', '2008-07-18 00:00:00', 399.00, 'https://m.media-amazon.com/images/M/MV5BYTFiMmNjNzgtMzA3NC00ZWI5LWIxMTUtYTNjZDI1YTYyZmJhXkEyXkFqcGdeQXVyNTU4NTE5MDQ@._V1_FMjpg_UX1000_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Life Is Beautiful', 'Italian', 'Drama', '1997-12-20 00:00:00', 299.00, 'https://upload.wikimedia.org/wikipedia/en/7/7c/Vitaebella.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'City of God', 'Portuguese', 'Crime', '2002-08-30 00:00:00', 349.00, 'https://m.media-amazon.com/images/M/MV5BMGU5OWEwZDItNmNkMC00NzZmLTk1YTctNzVhZTJjM2NlZTVmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Crouching Tiger, Hidden Dragon', 'Mandarin', 'Martial Arts', '2000-07-07 00:00:00', 399.00, 'https://m.media-amazon.com/images/M/MV5BNDdhMzMxOTctNDMyNS00NTZmLTljNWEtNTc4MDBmZTYxY2NmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Pulp Fiction', 'English', 'Crime', '1994-10-14 00:00:00', 399.00, 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'The Intouchables', 'French', 'Comedy', '2011-11-02 00:00:00', 299.00, 'https://m.media-amazon.com/images/M/MV5BODY0MDM1MDE2OV5BMl5BanBnXkFtZTcwMzEyMjkyNw@@._V1_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Pans Labyrinth', 'Spanish', 'Fantasy', '2006-10-11 00:00:00', 349.00, 'https://m.media-amazon.com/images/M/MV5BYzFjMThiMGItOWRlMC00MDI4LThmOGUtYTNlZGZiYWI1YjMyXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_FMjpg_UX1000_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Oldboy', 'Korean', 'Thriller', '2003-11-21 00:00:00', 349.00, 'https://m.media-amazon.com/images/M/MV5BMTk5NTEwMzI5OV5BMl5BanBnXkFtZTgwMDQ4ODUwMDE@._V1_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Cinema Paradiso', 'Italian', 'Drama', '1988-11-17 00:00:00', 299.00, 'https://m.media-amazon.com/images/M/MV5BM2FhYjEyYmYtMDI1Yy00YTdlLWI2NWQtYmEzNzAxOGY1NjY2XkEyXkFqcGdeQXVyNTA3NTIyNDg@._V1_.jpg');
+
+INSERT INTO `movies_T`(`movie_id`, `title`, `language`, `genre`, `release_date`, `rental_price`, `thumbnail_url`) VALUES (NULL, 'Slumdog Millionaire', 'English', 'Drama', '2008-11-12 00:00:00', 349.00, 'https://m.media-amazon.com/images/M/MV5BZmNjZWI3NzktYWI1Mi00OTAyLWJkNTYtMzUwYTFlZDA0Y2UwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg');
